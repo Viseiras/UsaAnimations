7d27a9e4fa025584f9c26d1312092fea1b2854c4
Viseiras <javi.vice@gmail.com>
2023-01-20 00:00:01 +0100

    Anyir Initial Commit

--- app/src/main/java/com/example/usaanimations/MainActivity.java
+++ app/src/main/java/com/example/usaanimations/MainActivity.java
@@ -1,10 +1,28 @@
 package com.example.usaanimations;
 
+import static android.content.ContentValues.TAG;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.animation.Animator;
+import android.animation.ArgbEvaluator;
+import android.animation.ValueAnimator;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.ColorFilter;
+import android.graphics.Paint;
+import android.graphics.PixelFormat;
+import android.graphics.Rect;
 import android.graphics.drawable.AnimationDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,12 +32,157 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ImageView imageView = findViewById(R.id.banyera);
+        LinearLayout ll = new LinearLayout(this);
+        ll.setOrientation(LinearLayout.VERTICAL);
+        ll.setBackgroundResource(R.drawable.background_degradado_principal);
 
+        ImageView imageView= new ImageView(this);
         imageView.setBackgroundResource(R.drawable.animacion_banyera);
 
         animacionbanyera=(AnimationDrawable) imageView.getBackground();
-
         animacionbanyera.start();
+        int ancho = 1300;
+        int alto = 800;
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(ancho, alto);
+        imageView.setLayoutParams(params);
+        ll.addView(imageView);
+
+
+
+        TextView tx = new TextView(this);
+        tx.setText("Temperatura");
+        tx.setPadding(60,20,60,20);
+        tx.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
+        tx.setBackgroundResource(R.drawable.background_degradado);
+        ll.addView(tx);
+
+        ProgressBar pb = new ProgressBar(this, null, android.R.attr.progressBarStyleHorizontal);
+        Drawable d = new ProgressDrawable();
+        pb.setProgressDrawable(d);
+
+        pb.setPadding(20, 20, 20, 0);
+
+        ll.addView(pb);
+
+
+        SeekBar.OnSeekBarChangeListener l = new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                int newProgress = pb.getMax() * progress / seekBar.getMax();
+                Log.d(TAG, "onProgressChanged " + newProgress);
+                pb.setProgress(newProgress);
+                if(progress==0)
+                {
+                    imageView.setBackgroundResource(R.drawable.animacion_fria);
+                    animacionbanyera=(AnimationDrawable) imageView.getBackground();
+                    animacionbanyera.start();
+                }
+                if(progress<5 && progress>0) {
+                    int colorFrom = (int)getColor(R.color.fondof);
+                    int colorTo = (int)getColor(R.color.fondoi);
+                    ValueAnimator colorAnimation = ValueAnimator.ofObject(new ArgbEvaluator(), colorFrom, colorTo);
+                    colorAnimation.setDuration(2000); // milliseconds
+                    colorAnimation.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+
+                        @Override
+                        public void onAnimationUpdate(ValueAnimator animator) {
+                            tx.setBackgroundColor((int) animator.getAnimatedValue());
+                        }
+
+                    });
+                    colorAnimation.start();
+                }
+                if(progress==5)
+                {
+                    tx.setBackgroundResource(R.drawable.background_degradado);
+                }
+                if(progress>5 && progress<10) {
+                    int colorFrom = (int)getColor(R.color.fondoi);
+                    int colorTo = (int)getColor(R.color.fondof);
+                    ValueAnimator colorAnimation = ValueAnimator.ofObject(new ArgbEvaluator(), colorFrom, colorTo);
+                    colorAnimation.setDuration(2000); // milliseconds
+                    colorAnimation.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+
+                        @Override
+                        public void onAnimationUpdate(ValueAnimator animator) {
+                            tx.setBackgroundColor((int) animator.getAnimatedValue());
+                        }
+
+                    });
+                    colorAnimation.start();
+                }
+                if(progress==10)
+                {
+                    imageView.setBackgroundResource(R.drawable.animacion_caliente);
+                    animacionbanyera=(AnimationDrawable) imageView.getBackground();
+                    animacionbanyera.start();
+                }
+                if(progress>1 && progress<10)
+                {
+                    imageView.setBackgroundResource(R.drawable.animacion_banyera);
+                    animacionbanyera=(AnimationDrawable) imageView.getBackground();
+                    animacionbanyera.start();
+                }
+            }
+        };
+
+        int[] maxs = {4, 10, 60, 110};
+
+            SeekBar sb = new SeekBar(this);
+            sb.setMax(maxs[1]);
+            sb.setOnSeekBarChangeListener(l);
+            sb.setPadding(60, 20, 60, 0);
+            ll.addView(sb);
+
+
+
+        setContentView(ll);
+    }
+
+}
+
+class ProgressDrawable extends Drawable {
+    private static final int NUM_RECTS = 10;
+    Paint mPaint = new Paint();
+
+    @Override
+    protected boolean onLevelChange(int level) {
+        invalidateSelf();
+        return true;
+    }
+
+    @Override
+    public void draw(Canvas canvas) {
+        int level = getLevel();
+        Rect b = getBounds();
+        float width = b.width();
+        for (int i = 0; i < NUM_RECTS; i++) {
+            float left = width * i / NUM_RECTS;
+            float right = left + 0.9f * width / NUM_RECTS;
+
+            mPaint.setColor((i + 1) * 10000 / NUM_RECTS <= level? 0xffFF8080:0xff8080FF);
+            canvas.drawRect(left, b.top, right, b.bottom, mPaint);
+        }
+    }
+
+    @Override
+    public void setAlpha(int alpha) {
+    }
+
+    @Override
+    public void setColorFilter(ColorFilter cf) {
+    }
+
+    @Override
+    public int getOpacity() {
+        return PixelFormat.TRANSLUCENT;
     }
 }
